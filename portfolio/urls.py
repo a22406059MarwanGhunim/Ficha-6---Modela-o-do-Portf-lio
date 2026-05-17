4ec74e82688cceba4863f7d0a9680bf59012edc6
a22406059MarwanGhunim <a22406059@alunos.ulht.pt>
2026-05-17 16:40:59 +0000

    ficha 8

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'portfolio'
 
 urlpatterns = [
-    path('', views.home_page_view, name='home'), # Página principal do portfólio
+    # --- HOME & LISTAGENS ---
+    path('', views.home_page_view, name='home'),
     path('licenciatura/', views.licenciatura_view, name='licenciatura'),
     path('professores/', views.professores_view, name='professores'),
     path('tecnologias/', views.tecnologias_view, name='tecnologias'),
@@ -13,4 +14,24 @@ urlpatterns = [
     path('competencias/', views.competencias_view, name='competencias'),
     path('tfcs/', views.tfcs_view, name='tfcs'),
     path('formacao/', views.formacao_view, name='formacao'),
+
+    # --- CRUD PROJETO ---
+    path('projeto/novo/', views.novo_projeto_view, name='novo_projeto'),
+    path('projeto/edita/<int:projeto_id>/', views.edita_projeto_view, name='edita_projeto'),
+    path('projeto/apaga/<int:projeto_id>/', views.apaga_projeto_view, name='apaga_projeto'),
+
+    # --- CRUD TECNOLOGIA ---
+    path('tecnologia/nova/', views.nova_tecnologia_view, name='nova_tecnologia'),
+    path('tecnologia/edita/<int:tecnologia_id>/', views.edita_tecnologia_view, name='edita_tecnologia'),
+    path('tecnologia/apaga/<int:tecnologia_id>/', views.apaga_tecnologia_view, name='apaga_tecnologia'),
+
+    # --- CRUD COMPETÊNCIA ---
+    path('competencia/nova/', views.nova_competencia_view, name='nova_competencia'),
+    path('competencia/edita/<int:competencia_id>/', views.edita_competencia_view, name='edita_competencia'),
+    path('competencia/apaga/<int:competencia_id>/', views.apaga_competencia_view, name='apaga_competencia'),
+
+    # --- CRUD FORMAÇÃO ---
+    path('formacao/nova/', views.nova_formacao_view, name='nova_formacao'),
+    path('formacao/edita/<int:formacao_id>/', views.edita_formacao_view, name='edita_formacao'),
+    path('formacao/apaga/<int:formacao_id>/', views.apaga_formacao_view, name='apaga_formacao'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Licenciatura, Professor, Tecnologia, Projeto, UC, Competencia, TFC, Formacao
+from .forms import ProjetoForm, TecnologiaForm, CompetenciaForm, FormacaoForm
+
+# --- HOME & LISTAGENS ---
+
+def home_page_view(request):
+    return render(request, 'portfolio/home.html')
 
 def licenciatura_view(request):
     return render(request, 'portfolio/licenciatura.html', {
@@ -10,6 +15,16 @@ def licenciatura_view(request):
 def professores_view(request):
     return render(request, 'portfolio/professores.html', {
         'professores': Professor.objects.all()
+    })
+
+def ucs_view(request):
+    return render(request, 'portfolio/ucs.html', {
+        'ucs': UC.objects.all()
+    })
+
+def tfcs_view(request):
+    return render(request, 'portfolio/tfcs.html', {
+        'tfcs': TFC.objects.all()
     })
 
 def tecnologias_view(request):
@@ -22,19 +37,9 @@ def projetos_view(request):
         'projetos': Projeto.objects.all()
     })
 
-def ucs_view(request):
-    return render(request, 'portfolio/ucs.html', {
-        'ucs': UC.objects.all()
-    })
-
 def competencias_view(request):
     return render(request, 'portfolio/competencias.html', {
         'competencias': Competencia.objects.all()
-    })
-
-def tfcs_view(request):
-    return render(request, 'portfolio/tfcs.html', {
-        'tfcs': TFC.objects.all()
     })
 
 def formacao_view(request):
@@ -42,5 +47,94 @@ def formacao_view(request):
         'formacoes': Formacao.objects.all()
     })
 
-def home_page_view(request):
-    return render(request, 'portfolio/home.html')
+
+# --- CRUD PROJETO ---
+
+def novo_projeto_view(request):
+    form = ProjetoForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('portfolio:projetos')
+    return render(request, 'portfolio/novo_projeto.html', {'form': form})
+
+def edita_projeto_view(request, projeto_id):
+    projeto = get_object_or_404(Projeto, id=projeto_id)
+    form = ProjetoForm(request.POST or None, request.FILES or None, instance=projeto)
+    if form.is_valid():
+        form.save()
+        return redirect('portfolio:projetos')
+    return render(request, 'portfolio/edita_projeto.html', {'form': form, 'projeto': projeto})
+
+def apaga_projeto_view(request, projeto_id):
+    projeto = get_object_or_404(Projeto, id=projeto_id)
+    projeto.delete()
+    return redirect('portfolio:projetos')
+
+
+# --- CRUD TECNOLOGIA ---
+
+def nova_tecnologia_view(request):
+    form = TecnologiaForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('portfolio:tecnologias')
+    return render(request, 'portfolio/nova_tecnologia.html', {'form': form})
+
+def edita_tecnologia_view(request, tecnologia_id):
+    tecnologia = get_object_or_404(Tecnologia, id=tecnologia_id)
+    form = TecnologiaForm(request.POST or None, request.FILES or None, instance=tecnologia)
+    if form.is_valid():
+        form.save()
+        return redirect('portfolio:tecnologias')
+    return render(request, 'portfolio/edita_tecnologia.html', {'form': form, 'tecnologia': tecnologia})
+
+def apaga_tecnologia_view(request, tecnologia_id):
+    tecnologia = get_object_or_404(Tecnologia, id=tecnologia_id)
+    tecnologia.delete()
+    return redirect('portfolio:tecnologias')
+
+
+# --- CRUD COMPETÊNCIA ---
+
+def nova_competencia_view(request):
+    form = CompetenciaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('portfolio:competencias')
+    return render(request, 'portfolio/nova_competencia.html', {'form': form})
+
+def edita_competencia_view(request, competencia_id):
+    competencia = get_object_or_404(Competencia, id=competencia_id)
+    form = CompetenciaForm(request.POST or None, instance=competencia)
+    if form.is_valid():
+        form.save()
+        return redirect('portfolio:competencias')
+    return render(request, 'portfolio/edita_competencia.html', {'form': form, 'competencia': competencia})
+
+def apaga_competencia_view(request, competencia_id):
+    competencia = get_object_or_404(Competencia, id=competencia_id)
+    competencia.delete()
+    return redirect('portfolio:competencias')
+
+
+# --- CRUD FORMAÇÃO ---
+
+def nova_formacao_view(request):
+    form = FormacaoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('portfolio:formacao')
+    return render(request, 'portfolio/nova_formacao.html', {'form': form})
+
+def edita_formacao_view(request, formacao_id):
+    formacao = get_object_or_404(Formacao, id=formacao_id)
+    form = FormacaoForm(request.POST or None, instance=formacao)
+    if form.is_valid():
+        form.save()
+        return redirect('portfolio:formacao')
+    return render(request, 'portfolio/edita_formacao.html', {'form': form, 'formacao': formacao})
+
+def apaga_formacao_view(request, formacao_id):
+    formacao = get_object_or_404(Formacao, id=formacao_id)
+    formacao.delete()
+    return redirect('portfolio:formacao')
